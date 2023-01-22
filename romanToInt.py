85fc3598a51cbf09f3aed9bdc9adaef8260c3655
Vahan Mkrtchyan <90754448+vmkrtchy@users.noreply.github.com>
2023-01-22 23:01:33 +0400

    Update romanToInt.py

--- romanToInt.py
+++ romanToInt.py
@@ -2,6 +2,7 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         dict1 = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
        
+    
         st = 0
         for i in range(len(s)):
             if i + 1 < len(s) and dict1[s[i]] < dict1[s[i+1]]:
