e48864be63b844c43fbb840439d6faeb74f65e31
Vahan Mkrtchyan <90754448+vmkrtchy@users.noreply.github.com>
2023-03-12 11:01:03 +0400

    Update romanToInt.py

--- romanToInt.py
+++ romanToInt.py
@@ -1,5 +1,5 @@
 #include hghjhhoikl;,
-
+#jrehgfhujk3hqeiufhrefgh
 class Solution:
     def romanToInt(self, s: str) -> int:
         dict1 = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
